10d3ebd85da2f812a7eb2bc8091c4ce67525dbf3
System Administrator <root@Chirags-MacBook-Air.local>
2022-03-04 11:59:39 +0530

    add branch

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,6 +4,10 @@ module.exports = async (req,res) => {
     console.log(req.body);
     await User.create(req.body,(error,user) => {
         if(error) {
+           // console.log("errors is here ===>",error.errors);
+            const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message);
+            req.session.validationErrors = validationErrors
+            console.log(error);
             return res.redirect('/auth/register');
         }
             res.redirect('/');
--- model/User.js
+++ model/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
+var uniqueValidator = require('mongoose-unique-validator');
 
 const UserSchema = new Schema({
     username: {
@@ -13,7 +14,7 @@ const UserSchema = new Schema({
         required : true
     }
 });
-
+UserSchema.plugin(uniqueValidator);
 UserSchema.pre('save',function(next){
     const user = this
     bcrypt.hash(user.password,10,(error,hash) => {
